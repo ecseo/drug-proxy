a7ce0059ed0d50578a42ba124453569e59af9ac7
secoam <130820280+ecseo@users.noreply.github.com>
2025-08-26 12:01:55 +0900

    Update component.js

--- api/component.js
+++ api/component.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
-  const { item_seq } = req.query; // 품목기준코드로 조회
+  const { Prduct } = req.query; // 의약품 제품 주성분 상세정보 조회는 제품명(한글)으로 조회
 
   const API_URL =
     "https://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService06/getDrugPrdtMcpnDtlInq06";
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   const params = new URLSearchParams({
     serviceKey: SERVICE_KEY,
     type: "json",
-    item_seq: item_seq,
+    Prduct: Prduct,
     numOfRows: "5",
     pageNo: "1",
   });
