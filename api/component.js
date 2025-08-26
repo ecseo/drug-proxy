b0f73ed98a25f945a1ac3f6dd4c4c3054475c343
secoam <130820280+ecseo@users.noreply.github.com>
2025-08-26 11:26:57 +0900

    Create component.js

---
+++ api/component.js
@@ -0,0 +1,33 @@
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  const { item_seq } = req.query; // 품목기준코드로 조회
+
+  const API_URL =
+    "https://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService06/getDrugPrdtMcpnDtlInq06";
+  const SERVICE_KEY = process.env.SERVICE_KEY;
+
+  const params = new URLSearchParams({
+    serviceKey: SERVICE_KEY,
+    type: "json",
+    item_seq: item_seq,
+    numOfRows: "5",
+    pageNo: "1",
+  });
+
+  try {
+    const response = await fetch(`${API_URL}?${params}`);
+    const text = await response.text();
+
+    let data;
+    try {
+      data = JSON.parse(text);
+    } catch {
+      return res.status(500).send({ error: "API returned XML", raw: text });
+    }
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
